f960cb1a56304113b7916df69946191d122caea0
Rundaingne <48364843+Rundaingne@users.noreply.github.com>
2022-08-04 15:40:35 -0600

    Created my initial code for collection view representable

---
+++ BookBestie/View/CollectionViewRepresentable.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewRepresentable.swift
+//  BookBestie
+//
+//  Created by Erich Kumpunen on 8/4/22.
+//
+
+import SwiftUI
+
+struct CollectionViewRepresentable: View {
+    var body: some View {
+        VStack {
+            Text("I am a representable")
+        }
+    }
+}
