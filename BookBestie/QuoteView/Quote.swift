a5f313c32bb2ed0f001dec7f164c73e1a7a43d36
Laura Gongaware <LauraGongaware@gmail.com>
2022-08-05 12:56:33 -0700

    Added files for quote API call and view

---
+++ BookBestie/QuoteView/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  BookBestie
+//
+//  Created by Laura Gongaware on 8/5/22.
+//
+
+import Foundation
+
+struct Quote: Codable {
+    let q: String
+    let a: String
+    
+}
