4b575be5da9206f6eba9d5e8b61b4a181bb42287
carmediem <carchiu@gmail.com>
2022-08-08 14:53:46 -0700

    updates to toggle for favoritebooklist

--- BookBestie/AppFlow.swift
+++ BookBestie/AppFlow.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct AppFlow: View {
     
     var body: some View {
+        LoginView()
         TabView {
             ContentView()
                 .tabItem {
---
+++ BookBestie/FavoriteBookList/FavKeyViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  FavKeyViewModel.swift
+//  BookBestie
+//
+//  Created by Carmen Chiu on 8/8/22.
+//
+
+import Foundation
+
+final class FavKeyViewModel {
+    private let FAV_KEY = "fav_key"
+    
+    func save(items: Set<UUID>) {
+        let array = Array(items)
+        UserDefaults.standard.set(array, forKey: FAV_KEY)
+    }
+    func load() -> Set<UUID> {
+        let array = UserDefaults.standard.array(forKey: FAV_KEY) as? [UUID] ?? [UUID]()
+        return Set(array)
+    }
+}
